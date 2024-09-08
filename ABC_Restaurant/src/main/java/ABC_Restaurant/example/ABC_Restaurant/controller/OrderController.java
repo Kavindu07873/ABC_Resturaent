b096b8c5a27eedec9df2720f761f86116e9d0203
Kavindu07873 <dilshankavindu07715@gmail.com>
2024-09-09 02:27:47 +0530

    create order

--- ABC_Restaurant/src/main/java/ABC_Restaurant/example/ABC_Restaurant/controller/OrderController.java
+++ ABC_Restaurant/src/main/java/ABC_Restaurant/example/ABC_Restaurant/controller/OrderController.java
@@ -1,4 +1,13 @@
 package ABC_Restaurant.example.ABC_Restaurant.controller;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@Log4j2
+@RequestMapping("/order")
 public class OrderController {
 }
